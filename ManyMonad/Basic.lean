746b5f07c6314c0fac7bd1240659f9fa973296ad
Seasawher <tellagoodstory@icloud.com>
2024-06-07 22:39:51 +0900

    add Basic

--- ManyMonad/Basic.lean
+++ ManyMonad/Basic.lean
@@ -1,1 +1,34 @@
-def hello := "world"
+/-- lazy analogy of list.
+see <https://lean-lang.org/functional_programming_in_lean/monads/arithmetic.html#nondeterministic-search> -/
+inductive Many (α : Type) where
+  | none : Many α
+  | more : α → (Unit → Many α) → Many α
+
+def Many.one (x : α) : Many α := Many.more x (fun () => Many.none)
+
+def Many.union : Many α → Many α → Many α
+  | Many.none, ys => ys
+  | Many.more x xs, ys => Many.more x (fun () => union (xs ()) ys)
+
+def Many.fromList : List α → Many α
+  | [] => Many.none
+  | x :: xs => Many.more x (fun () => fromList xs)
+
+def Many.take : Nat → Many α → List α
+  | 0, _ => []
+  | _ + 1, Many.none => []
+  | n + 1, Many.more x xs => x :: (xs ()).take n
+
+def Many.takeAll : Many α → List α
+  | Many.none => []
+  | Many.more x xs => x :: (xs ()).takeAll
+
+def Many.bind : Many α → (α → Many β) → Many β
+  | Many.none, _ =>
+    Many.none
+  | Many.more x xs, f =>
+    (f x).union (bind (xs ()) f)
+
+instance : Monad Many where
+  pure := Many.one
+  bind := Many.bind
